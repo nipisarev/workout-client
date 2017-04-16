2f6755229f10d575830fd4c6bb82e4ae6cefedc5
Anton Vaschenko <vashchenko-01@mail.ru>
2017-04-16 15:16:43 +0300

    any fixes

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
         loader: 'babel-loader',
         query: {
           presets: ['react', 'es2015', 'stage-0'],
-          plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy'],
+          plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy', 'transform-react-remove-prop-types'],
         }
       },
       {
