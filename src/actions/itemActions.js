15d8aef89af2e3c7ec2d6c4dfbcc317d11bdaa14
Nickolay Pisarev <n.pisarev@hostcomm.ru>
2017-04-26 01:45:26 +0300

    and forget add actions

--- src/actions/itemActions.js
+++ src/actions/itemActions.js
@@ -1,3 +1,17 @@
-/**
- * Created by npisarev on 26/04/2017.
- */
+export const action = {
+  INCREMENT: 'INCREMENT',
+  DECREMENT: 'DECREMENT',
+  RESET: 'RESET'
+};
+
+export function increment(vote) {
+  return {...vote, rating: vote.rating + 1};
+}
+
+export function decrement(vote) {
+  return {...vote, rating: vote.rating - 1};
+}
+
+export function reset(vote) {
+  return {...vote, rating: 0};
+}
